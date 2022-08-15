20897cf845ffd580d38970dc39297591f552401e
Fatma Kiran <ftmkrntest@gmail.com>
2022-08-15 18:12:02 -0400

    c1

---
+++ src/Deneme2.java
@@ -0,0 +1,8 @@
+public class Deneme2 {
+    public static void main(String[] args) {
+        System.out.println("Deneme2 calisma");
+        System.out.println("Deneme3 calisma");
+        System.out.println("Deneme4 calisma");
+        System.out.println("fatma" );
+    }
+}
